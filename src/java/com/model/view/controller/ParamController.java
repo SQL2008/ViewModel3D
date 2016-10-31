39c6a2c1085f30aa00e1e3fcef80faa220401da8
SQL2008 <admin@vincode.info>
2016-10-31 20:02:09 +0300

    development

--- src/java/com/model/view/controller/ParamController.java
+++ src/java/com/model/view/controller/ParamController.java
@@ -1,14 +1,16 @@
 package com.model.view.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+@Component
 @Controller
 public class ParamController {
     @RequestMapping(value = "/param")
     public String greetingForm(Model model) {
-        //model.addAttribute("greeting", new Greeting());
+        model.addAttribute("message", "Test message!");
         return "param";
     }
 }
